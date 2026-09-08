2e181fe06837e935ca091d084161e462d2c1c684
arindadeogratius-pixel <arindadeogratius@gmail.com>
2026-09-08 17:53:59 +0300

    population assignment

--- Chapter_02/population.cpp
+++ Chapter_02/population.cpp
@@ -4,10 +4,11 @@
 // One death every 13 seconds
 // One new immigrant every 45 seconds
 // Write a program to display the population for each of the next five years. Assume the current population is 312032486 and one year has 365 days.
+
 #include <iostream>
 
 int main() {
-    
+
     long population = 312032486;
     const long secondsPerYear = 365L * 24 * 60 * 60; // 31,536,000
 
